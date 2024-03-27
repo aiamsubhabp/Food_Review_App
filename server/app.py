75c96ec753cd6574ec418a2ac9f6060045d85c0a
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-03-27 13:20:30 -0400

    create models

--- server/app.py
+++ server/app.py
@@ -1,4 +1,10 @@
-from config import app
+from config import app, db, api
+from flask import Flask, make_response, session, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from models import Customer, Restaurant, Review
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
---
+++ server/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from config import db
+
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+
+    reviews = db.relationship('Review', back_populates = 'customer')
+
+    def __repr__(self):
+        return f'<Customer {self.id}, {self.username}>'
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    cuisine_type = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates = 'restaurant')
+
+    def __repr__(self):
+        return f'<Restaurant {self.id}, {self.name}, {self.cuisine_type}'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key = True)
+    rating = db.Column(db.String)
+    review = db.Column(db.String)
+    image = db.Column(db.String, nullable = True)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    customer = db.relationship('User', back_populates = 'reviews')
+    restaurant = db.relationship('Restaurant', back_populates = 'reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.rating}, {self.review}, {self.customer.username}, {self.restaurant.name}>'
